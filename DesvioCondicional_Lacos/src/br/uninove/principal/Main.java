ad2494070ffec83b4ed4ca74bd26bd4ecdb47ff4
Prof. Thiago G. Traue <t.traue@gmail.com>
2023-09-13 21:38:00 -0300

    Update Main.java

--- DesvioCondicional_Lacos/src/br/uninove/principal/Main.java
+++ DesvioCondicional_Lacos/src/br/uninove/principal/Main.java
@@ -1,5 +1,3 @@
-package br.uninove.principal;
-
 import java.util.Scanner;
 
 public class Main {
@@ -13,49 +11,132 @@ public class Main {
             }
         */
         
-        float nota1, nota2, nota3, media;
+        // float nota1, nota2, nota3, media;
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Informe as notas do(a) aluno(a): ");
+        // System.out.println("Informe as notas do(a) aluno(a): ");
         
-        System.out.print(" - Nota 1: ");
-        nota1 = sc.nextFloat();
+        // System.out.print(" - Nota 1: ");
+        // nota1 = sc.nextFloat();
         
-        System.out.print("\n - Nota 2: ");
-        nota2 = sc.nextFloat();
+        // System.out.print("\n - Nota 2: ");
+        // nota2 = sc.nextFloat();
         
-        System.out.print("\n - Nota 3: ");
-        nota3 = sc.nextFloat();
+        // System.out.print("\n - Nota 3: ");
+        // nota3 = sc.nextFloat();
         
-        media = (nota1 + nota2 + nota3) / 3;
+        // media = (nota1 + nota2 + nota3) / 3;
        
-        System.out.println("\nMédia: " + media);      
+        // System.out.println("\nMédia: " + media);      
         
-        if (media >= 6) {
-            System.out.println("Aprovado!");
-        } else {
-            if (media < 4) {
-                System.out.println("Reprovado!");
-            } else {
-                System.out.print("\nInforme a nota do exame: ");
-                float exame = sc.nextFloat();
-                if (exame >= 6) {
-                    System.out.println("Aprovado!");
-                } else {
-                    System.out.println("Reprovado!");
-                }
-            }
+        // if (media >= 6) {
+        //     System.out.println("Aprovado!");
+        // } else {
+        //     if (media < 4) {
+        //         System.out.println("Reprovado!");
+        //     } else {
+        //         System.out.print("\nInforme a nota do exame: ");
+        //         float exame = sc.nextFloat();
+        //         if (exame >= 6) {
+        //             System.out.println("Aprovado!");
+        //         } else {
+        //             System.out.println("Reprovado!");
+        //         }
+        //     }
             
-        }
-        
-        /*
-        Altere esse código para que se o aluno tirou 6 ou mais, ele está aprovado.
-        Entretanto, se ele tirou entre 4 e 5.9 ele poderá realizar um exame (nova 
-        nota a ser lida). 
-        Se ele tirar mais que 6 no exame, está aprovado, caso contrário, reprovado.
-        Se a média dele for menor que 4, ele não terá direito ao exame e está 
-        automáticamente reprovado.
-        >> 20.55 resolveremos
-        */
+        // }
+
+      //------------------------Comparando Strings----------------
+      
+      String nome1, nome2;
+      
+      System.out.print("Informe o primeiro nome: ");
+      nome1 = sc.next();
+      
+      System.out.print("Informe o segundo nome: ");
+      nome2 = sc.next();
+      
+      System.out.println("Você digitou: " + nome1 + " e " + nome2);
+
+      if (nome1.equals(nome2)) {
+        System.out.println("Os nomes digitados são iguais!");
+      } else {
+        System.out.println("Os nomes digitados são diferentes!");
+      }
+
+
+      if (nome1.equalsIgnoreCase(nome2)) {
+        System.out.println("Os nomes digitados são iguais, independente da caixa!");
+      } else {
+        System.out.println("Os nomes digitados são diferentes, independente da caixa!");
+      }
+
+      //Comparação com ternário:
+      System.out.println(nome1.equals(nome2) ? "Nomes iguais" : "Nomes diferentes!");
+
+
+      //------------- Operador Switch.. case ----------------
+      int finalPlaca = 1;
+      switch (finalPlaca) {
+        case 1:
+        case 2:
+          System.out.println("Rodízio de segunda-feira");
+          break;
+        case 3:
+        case 4:
+          System.out.println("Rodízio de terça-feira");
+          break;
+        case 5:
+        case 6:
+          System.out.println("Rodízio de quarta-feira");
+          break;
+        case 7:
+        case 8:
+          System.out.println("Rodízio de quinta-feira");
+          break;
+        case 9:
+        case 0:
+          System.out.println("Rodízio de sexta-feira");
+          break;
+        default:
+          System.out.println("Placa inválida!");
+          break;
+      }
+
+      //---------------------- Laços ----------------------
+      //Laços FOR: para i de 0 até n, faça
+
+
+      //condição de início; condição de parada; condição de incremento
+      for (int i = 10; i >= 0; i--) { //i++ é o mesmo que i = i + 1
+         System.out.println("For: I = " + i);
+      }
+
+      System.out.println("----------------");
+      
+      int j = 0;
+      while (j <= 10) {
+        System.out.println("While: J = " + j);
+        j++;
+      }
+
+      System.out.println("----------------");
+
+      int k = 0;
+      do {
+        System.out.println("Do..While: K = " + k);
+        k += 2; //é o mesmo que k = k + 2
+      } while (k <= 20);
+
+      /*
+      Desafio: Use os três tipos de laços para criar um algoritmo que some
+      n valores, sendo n, dado pelo usuário:
+      Entrada: 5
+      Saída: 1 + 2 + 3 + 4 + 5 = 15
+
+      Entrada: 6
+      Saída: 1 + 2 + 3 + 4 + 5 + 6 = 21
+      ...
+      */
     }
 }
