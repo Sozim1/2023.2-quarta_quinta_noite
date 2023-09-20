3524785177a4ca8dc9b7c957878120823581aa4b
Prof. Thiago G. Traue <t.traue@gmail.com>
2023-09-20 20:41:19 -0300

    Update Principal.java

--- Vetores/src/projeto/Principal.java
+++ Vetores/src/projeto/Principal.java
@@ -2,7 +2,6 @@ package projeto; //não coloque essa linha do replit
 
 import java.util.Scanner;
 
-
 class Principal {
 
     public static void main(String[] args) {
@@ -28,29 +27,28 @@ class Principal {
             a mensagem: "Nao encontrado!"
          */
         //SOLUÇÃO
-        
         //declatação do vetor de 15 posições:
         int tamanhoVetor, valorBuscado, aux;
- 
+
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.print("Digite tamanho do vetor desejado: ");
         tamanhoVetor = sc.nextInt();
-        
+
         int valores[] = new int[tamanhoVetor];
-        
+
         System.out.println("Comece a digitar os valores....");
-        
+
         //leitura dos valores:
         for (int i = 0; i < tamanhoVetor; i++) {
             System.out.print("Valor da " + (i + 1) + "ª posição: ");
             valores[i] = sc.nextInt();
         }
-        
+
         //pergunta ao usuário qual valor a ser buscado:
         System.out.print("\nQual valor você quer buscar: ");
         valorBuscado = sc.nextInt();
-        
+
         //iniamos a busca:
         aux = 0;
         for (int i = 0; i < tamanhoVetor; i++) {
@@ -59,14 +57,13 @@ class Principal {
                 aux++;
             }
         }
-        
+
         if (aux == 0) {
-            System.out.println("Não encontrei nenhuma vez o "  + valorBuscado);
+            System.out.println("Não encontrei nenhuma vez o " + valorBuscado);
         } else {
             System.out.println("Encontrei " + aux + " vezes o valor " + valorBuscado);
         }
-        
-        
+
         /*
             2. Faça um programa em Java que leia N números inteiros ("N" deve ser lido)
             e imprima a lista de valores informando se cada posição armazena 
@@ -82,7 +79,89 @@ class Principal {
             inteiros. Imprimir o maior e o menor, sem ordenar, o 
             percentual de números pares e a média dos elementos do vetor.
 
-        */
+         */
+        //RESOLUÇÕES
+        //EX2:
+        System.out.print("Informe quantos números serão lidos: ");
+        int n = sc.nextInt();
+        int vet[] = new int[n];
+        System.out.println("Comece a digitar os valores do vetor...:");
 
+        //Leitura dos dados do vetor
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite o valor vet[" + i + "]: ");
+            vet[i] = sc.nextInt();
+        }
+
+        //verificação par/impar de cada posição doo vetor
+        for (int i = 0; i < n; i++) {
+            if (vet[i] % 2 == 0) {
+                System.out.println("[" + i + "] -> Valor: " + vet[i] + " -> PAR");
+            } else {
+                System.out.println("[" + i + "] -> Valor: " + vet[i] + " -> ÍMPAR");
+            }
+        }
+
+        //EX3:
+        int v[] = new int[10];
+        int w[] = new int[10];
+        System.out.println("Digite os valores do vetor a calcular o fatorial...:");
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Valor v[" + i + "]: ");
+            v[i] = sc.nextInt();
+
+            int fatorial = 1;
+            if (v[i] == 1) {
+                w[i] = 1;
+            } else {
+                for (int j = 1; j < v[i]; j++) {
+                    fatorial *= j + 1;
+                }
+                w[i] = fatorial;
+            }
+        }
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("v[" + i + "] = " + v[i] + " => w[" + i + "] = " + w[i]);
+        }
+        
+        //EX4: 
+        int k[] = new int[10];
+        int menor, maior;
+        float media = 0, pares = 0;
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Valor k[" + i + "]: ");
+            k[i] = sc.nextInt();
+        }
+        
+        maior = menor = k[0];
+        
+        for (int i = 0; i < 10; i++) {
+            //busca do maior:
+            if (k[i] > maior) {
+                maior = k[i];
+            }
+            
+            //busca do menor:
+            if (k[i] < menor) {
+                menor = k[i];
+            }
+            
+            //contagem dos pares
+            if (k[i] % 2 == 0) {
+                pares++;
+            }
+            
+            //soma para a média
+            media += k[i];
+        }
+        
+        media /= 10;
+        
+        System.out.println("Menor valor encontrado: " + menor);
+        System.out.println("Maior valor encontrado: " + maior);
+        System.out.println("Média dos valores: " + media);
+        System.out.println("Porcentagem de valores pares: " + (pares / 10) * 100 + "%");
     }
 }
